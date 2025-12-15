fd6a077ea1259f74c02f59d8c12ae4114e35f064
Trieu Vo <minhtrieuvo600@gmail.com>
2025-12-15 14:32:43 +0700

    Minor fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Headers } from '@nestjs/common';
 import { AppService } from './app.service';
 import { TransactionService } from './transaction.service';
 import { ProcessTextDto } from './sentiment.dto';
@@ -8,15 +8,23 @@ export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly transactionService: TransactionService,
-  ) {}
+  ) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
+  @Post('ping')
+  async ping(@Body() body: Record<string, any>, @Headers() headers: Record<string, string>) {
+    console.log(new Date(), 'Request Headers: ', headers);
+    console.log(new Date(), 'Request Body: ', body);
+    return { message: 'pong' };
+  }
+
   @Post('process-text')
-  async processText(@Body() dto: ProcessTextDto) {
+  async processText(@Body() dto: ProcessTextDto, @Headers() headers: Record<string, string>) {
+    console.log(new Date(), 'Request Headers: ', headers);
     const result = await this.transactionService.processText(dto.input, dto.type || 'auto');
     console.log(new Date(), 'Result: ', result.success ? 'Success' : 'Failed');
     return result;
